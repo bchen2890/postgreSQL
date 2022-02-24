a3b2793a4c66cf1420b54ddbfaa15479c3a6e55f
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-02-24 01:17:20 +0100

    UNION, INTERSECT and EXCEPT

--- Basic/7_operator.sql
+++ Basic/7_operator.sql
@@ -1,8 +1,6 @@
-DROP TABLE IF EXISTS orders;
-DROP TABLE IF EXISTS orders2;
-DROP TABLE IF EXISTS orders3;
 
-DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS popular_products;
+DROP TABLE IF EXISTS top_products;
 
 CREATE TABLE popular_products (
     prod_id INT PRIMARY KEY,
@@ -24,11 +22,23 @@ CREATE TABLE top_products (
 
 INSERT INTO top_products (prod_id, name)
 VALUES
-    (1, 'Skirt'),
-    (2, 'Cacao'),
-    (3, 'Pullover'),
-    (4, 'Brush'),
-	(5, 'Avocado');
-	
-SELECT * FROM popular_products UNION SELECT * FROM top_products;
+    (1, 'Apple'),
+	(2, 'Cake'),
+    (6, 'Skirt'),
+    (7, 'Cacao'),
+    (8, 'Pullover'),
+    (9, 'Brush'),
+	(10, 'Avocado');
+
+--UNION ALL combines results sets from the tables
 SELECT * FROM popular_products UNION ALL SELECT * FROM top_products;
+
+--UNION combines results sets and remove duplicate rows
+SELECT * FROM popular_products UNION SELECT * FROM top_products;
+
+--INTERSECT returns the rows that appear in both result sets
+SELECT * FROM popular_products INTERSECT SELECT * FROM top_products;
+
+--EXCEPT return the rows in the first query that does not appear in the output of the second query.
+SELECT * FROM popular_products EXCEPT SELECT * FROM top_products;
+SELECT * FROM top_products EXCEPT SELECT * FROM popular_products;
