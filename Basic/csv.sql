82b974c44e4fe3dbd71dce21416d81f107388d5c
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-04-10 21:42:33 +0200

    Export to csv

--- Basic/csv.sql
+++ Basic/csv.sql
@@ -1,4 +1,13 @@
+\i 1_create.sql
+
 COPY accounts (username,password,created_on,email,birth_date,first_name,last_name)
 FROM 'E:/postgresql/postgreSQL/Basic/account.csv'
 DELIMITER ','
 CSV HEADER;
+
+\i 2_insert.sql
+COPY accounts 
+TO 'E:/postgresql/postgreSQL/Basic/exportaccount.csv' 
+DELIMITER ',' 
+CSV HEADER;
+
