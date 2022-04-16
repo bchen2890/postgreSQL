783d848a414df81b3c7fda48761611acfcd56661
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-04-16 22:16:58 +0200

    TRUNCATE

--- Basic/5_delete.sql
+++ Basic/5_delete.sql
@@ -15,3 +15,21 @@ SELECT * FROM roles;
 DELETE FROM roles;
 SELECT * FROM roles;
 
+
+--TRUNCATE TABLE remove all data in a more efficient way
+\i 2_insert.sql
+TRUNCATE TABLE roles;
+
+--Remove from multiple tables
+\i 2_insert.sql
+TRUNCATE TABLE roles, accounts;
+
+--Reset the value in the identity column
+\i 2_insert.sql
+TRUNCATE TABLE roles
+RESTART IDENTITY;
+
+--Remove from a table that has foreign key references
+\i 2_insert.sql
+TRUNCATE TABLE accounts CASCADE;
+
